482ce4cb5d58dc8c36bf4ea37f9957cfee93de91
Grechko Maria <grechko.maria2003@gmail.com>
2023-05-12 09:05:34 +0300

    Add rental and user checks before sending email

--- app/workers/confirmation_email_worker.rb
+++ app/workers/confirmation_email_worker.rb
@@ -3,6 +3,9 @@ class ConfirmationEmailWorker
 
   def perform(rental_id)
     rental = Rental.find_by(id: rental_id)
-    RentalMailer.with(rental: rental).rental_confirmation_email.deliver_now
+
+    if rental.present?
+      RentalMailer.with(rental: rental).rental_confirmation_email.deliver_now
+    end
   end
 end
--- app/workers/request_owner_access_worker.rb
+++ app/workers/request_owner_access_worker.rb
@@ -3,6 +3,9 @@ class RequestOwnerAccessWorker
 
   def perform(user_id)
     user = User.find_by(id: user_id)
-    UserMailer.request_owner_access(user).deliver_now
+
+    if user.present?
+      UserMailer.request_owner_access(user).deliver_now
+    end
   end
 end
