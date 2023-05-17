0a0e1253f4bd274a2050a430d5c78e31218dccf3
Grechko Maria <grechko.maria2003@gmail.com>
2023-05-17 15:11:20 +0300

    Fix service methods

--- app/services/rental_price_calculator.rb
+++ app/services/rental_price_calculator.rb
@@ -3,16 +3,9 @@ class RentalPriceCalculator < Patterns::Service
     @start_date = start_date
     @end_date = end_date
     @product_price = product_price
-    @result = nil
   end
 
   def call
-    @result = total_price
-  end
-
-  private
-
-  def total_price
     (@end_date - @start_date + 1).to_i * @product_price
   end
 end
