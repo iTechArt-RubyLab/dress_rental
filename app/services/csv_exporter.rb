043cc73cf98715a26cc47ee895c6195eb97b8af6
Grechko Maria <grechko.maria2003@gmail.com>
2023-05-21 14:06:59 +0300

    Add rentals price to csv file

--- app/services/csv_exporter.rb
+++ app/services/csv_exporter.rb
@@ -2,10 +2,10 @@ require 'csv'
 class CsvExporter < Patterns::Service
   def self.call(rentals)
     CSV.generate do |csv|
-      csv << ['ID', 'Start date', 'End date', 'Product name', 'Customer email', 'Rental status']
+      csv << ['ID', 'Start date', 'End date', 'Product name', 'Customer email', 'Rental status', 'Total price, $']
   
       rentals.each do |rental|
-        csv << [rental.id, rental.start_date, rental.end_date, rental.product.name, rental.user.email, rental.status]
+        csv << [rental.id, rental.start_date, rental.end_date, rental.product.name, rental.user.email, rental.status, rental.total_price]
       end
     end
   end
