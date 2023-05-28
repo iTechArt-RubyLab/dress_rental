7f05f88a217c7ebc8d8f8e9214558b8f604e4574
Grechko Maria <grechko.maria2003@gmail.com>
2023-05-28 08:20:51 +0300

    Add self.search and mapping to Product model

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,13 +1,32 @@
 class Product < ApplicationRecord
+  require 'elasticsearch/model'
+  include Elasticsearch::Model
+  include Elasticsearch::Model::Callbacks
   DEFAULT_PHOTO_URL = 'default-object-photo.png'.freeze
   has_many :product_categories
   has_many :categories, through: :product_categories
   belongs_to :salon
   has_many :rentals
   has_many :users, through: :rentals
-  has_one_attached :photo
+  # has_one_attached :photo
 
   validates :name, :price, :description, presence: true
+
+  mapping do
+    indexes :name, type: :text
+  end
+
+  def self.search(query)
+    self.__elasticsearch__.search(
+      {
+        query: {
+          match: {
+            name: query
+          }
+        }
+      }
+    )
+  end
 
   def photo_url(_size = :medium)
     if photo.attached?
