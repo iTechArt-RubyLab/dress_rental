d20795ab930946ad02e699cd8876400cc1d99d0c
Grechko Maria <grechko.maria2003@gmail.com>
2023-05-30 14:45:10 +0300

    Remove extra methods from admin controller

--- app/controllers/admin/rentals_controller.rb
+++ app/controllers/admin/rentals_controller.rb
@@ -1,34 +1,16 @@
 module Admin
   class RentalsController < AdminController
-    before_action :set_rental, only: %i[show update destroy]
+    before_action :set_rental, only: %i[edit update destroy]
 
     def index
       @rentals = Rental.all
     end
 
-    def new
-      @rental = Rental.new
-    end
-
-    def show; end
-
-    def create
-      @rental = current_user.rentals.new(rental_params)
-      if @rental.save
-        redirect_to @rental
-      else
-        redirect_to new_rental_path
-        flash[:alert] = @rental.errors.full_messages.join("\n")
-      end
-    end
-
-    def edit
-      @rental = Rental.find(params[:id])
-    end
+    def edit; end
 
     def update
       if @rental.update(rental_params)
-        redirect_to @rental
+        redirect_to @rental, notice: 'Rental was successfully updated'
       else
         redirect_to edit_rental_path
         flash[:alert] = @rental.errors.full_messages.join("\n")
@@ -36,9 +18,8 @@ module Admin
     end
 
     def destroy
-      @rental.destroy
-      respond_to do |format|
-        format.html { redirect_to admin_rentals_path, notice: 'Rental was successfully destroyed' }
+      if @rental.destroy
+        redirect_to admin_rentals_path, notice: 'Rental was successfully destroyed' 
       end
     end
 
